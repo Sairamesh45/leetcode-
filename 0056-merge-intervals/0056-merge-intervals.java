56dff8bf40214fcf6f44c4e6bee56c91661ce264
Sai ramesh <sairameshdevaguptapu@gmail.com>
2026-08-22 23:29:25 +0530

    Time: 13 ms (5.81%), Space: 48.9 MB (85.78%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a,b) -> Integer.compare(a[0],b[0]));
+        ArrayList<int[]> arr = new ArrayList<>();
+        int i = 0;
+        while(i+1 < intervals.length && intervals[i][1] < intervals[i+1][0])
+        {
+            arr.add(new int[]{intervals[i][0],intervals[i][1]});
+            i++;
+        }
+
+        int start = intervals[i][0];
+        int end = intervals[i][1];
+        while(i+1 < intervals.length && end >= intervals[i+1][0])
+        {
+            start = Math.min(start, intervals[i+1][0]);
+            end = Math.max(end, intervals[i+1][1]);
+            i++;
+        }
+
+        arr.add(new int[]{start,end});
+        i++;
+        while(i < intervals.length)
+        {
+            start = intervals[i][0];
+            end = intervals[i][1];
+            i++;
+
+            while(i < intervals.length && end >= intervals[i][0])
+            {
+                end = Math.max(end, intervals[i][1]);
+                i++;
+            }
+
+            arr.add(new int[]{start,end});
+        }
+
+        int ans[][] = new int[arr.size()][2];
+
+        for(int j = 0; j < ans.length; j++)
+        {
+            ans[j] = arr.get(j);
+        }
+
+        return ans;
+    }
+}
