3bcc30d95cc27d6dc1669b0534e4b61af9a01f60
Sai ramesh <sairameshdevaguptapu@gmail.com>
2026-08-26 00:08:01 +0530

    Time: 28 ms (22.41%), Space: 47.1 MB (16.42%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -3,26 +3,31 @@ class Solution {
         HashMap<Character,Integer> map = new HashMap<>();
 
         int l = 0, r = 0;
-        int max = 0, maxFreq = 0;
+
+        int max = 0;
+        int maxFreq = 0;
 
         while(r < s.length())
         {
             char ch = s.charAt(r);
             map.put(ch, map.getOrDefault(ch,0)+1);
+            maxFreq = Math.max(maxFreq, map.get(ch));
 
-            maxFreq = Math.max(maxFreq,map.get(ch));
-
-            while((r-l+1) - maxFreq > k)
+            while(r - l + 1 > (maxFreq + k))
             {
                 char ch1 = s.charAt(l);
+
                 map.put(ch1, map.get(ch1) - 1);
+
                 if(map.get(ch1) == 0) map.remove(ch1);
+
                 l++;
             }
 
-            max = Math.max(max,r-l+1);
+            max = Math.max(max, r - l + 1);
             r++;
         }
+
         return max;
     }
 }
