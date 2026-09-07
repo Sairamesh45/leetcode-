b21499643476b7b840f41a2bf7241d4650a218c0
Sai ramesh <sairameshdevaguptapu@gmail.com>
2026-09-07 23:10:00 +0530

    Time: 2 ms (88.7%), Space: 44 MB (84.3%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,56 +1,57 @@
 class Solution {
-    public int orangesRotting(int[][] grid) {
-        Queue<int[]> q = new LinkedList<>();
+    public int orangesRotting(int[][] grid) { 
+        int[][] dirs = {
+            {-1, 0},
+            {1, 0},
+            {0, -1},
+            {0, 1}
+        };
         int fresh = 0;
 
-        for(int i = 0; i < grid.length; i++)
+        int n = grid.length;
+        int m = grid[0].length;
+        Queue<int[]> q = new LinkedList<>();
+
+        for(int i = 0; i < n; i++)
         {
-            for(int j = 0; j < grid[i].length; j++)
+            for(int j = 0; j < m; j++)
             {
                 if(grid[i][j] == 2)
+                {
                     q.add(new int[]{i,j});
-                else if(grid[i][j] == 1)
-                    fresh++;
+                }
+                else if(grid[i][j] == 1) fresh++;
             }
         }
-
-        int dirs[][] = {
-            {1, 0},
-            {-1, 0},
-            {0, 1},
-            {0, -1}
-        };
-
         int minutes = 0;
 
         while(!q.isEmpty() && fresh > 0)
         {
             int size = q.size();
 
-            for(int i = 0; i < size; i++)
+            for(int k = 0; k < size; k++){
+            int curr[] = q.poll();
+            int r = curr[0];
+            int c = curr[1];
+
+            for(int dir[] : dirs)
             {
-                int []arr = q.poll();
-                int r = arr[0];
-                int c = arr[1];
+                int nr = r + dir[0];
+                int nc = c + dir[1];
 
-                for(int dir[] : dirs)
+                if(nr >= 0 && nr < n && nc >= 0 && nc < m && grid[nr][nc] == 1)
                 {
-                    int nr = r + dir[0];
-                    int nc = c + dir[1];
-
-                    if(nr >= 0 && nr < grid.length && nc >= 0 && nc < grid[0].length && grid[nr][nc] == 1)
-                    {
-                        fresh--;
-                        grid[nr][nc] = 2;
-                        q.add(new int[]{nr,nc});
-                    }
+                    fresh--;
+                    grid[nr][nc] = 2;
+                    q.add(new int[]{nr,nc});
                 }
             }
+            }
+            
             minutes++;
         }
-
+        
         if(fresh > 0) return -1;
-
         return minutes;
     }
 }
