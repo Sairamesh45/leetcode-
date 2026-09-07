8f161e1560fb42064e3df8ebfea348ba55091853
Sai ramesh <sairameshdevaguptapu@gmail.com>
2026-09-07 22:53:58 +0530

    Time: 3 ms (26.33%), Space: 49.8 MB (9.02%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,49 +1,48 @@
 class Solution {
-    int count = 0;
     public int findCircleNum(int[][] isConnected) {
         List<List<Integer>> graph = new ArrayList<>();
 
         int n = isConnected.length;
-        for(int i = 1; i <= n; i++)
+        int m = isConnected[0].length;
+
+        for(int i = 0; i < n; i++)
         {
             graph.add(new ArrayList<>());
         }
 
         for(int i = 0; i < n; i++)
         {
-            for(int j = 0; j < isConnected[i].length; j++)
+            for(int j = i+1; j < m; j++)
             {
-                if(isConnected[i][j] == 1)
-                {
-                    graph.get(i).add(j);
-                    graph.get(j).add(i);
+                if(isConnected[i][j] == 1){
+                graph.get(i).add(j);
+                graph.get(j).add(i);
                 }
             }
         }
 
         boolean visited[] = new boolean[n];
+        int count = 0;
 
         for(int i = 0; i < n; i++)
         {
             if(!visited[i])
             {
-                dfs(graph, visited, i);
+                dfs(visited, graph, i);
                 count++;
             }
         }
-
+        
         return count;
+        
     }
 
-    public void dfs(List<List<Integer>> graph, boolean []visited, int node)
+    public void dfs(boolean visited[], List<List<Integer>> graph, int node)
     {
         visited[node] = true;
 
-        for(int nei : graph.get(node))
-        {
-            if(!visited[nei]){
-                dfs(graph, visited, nei);
-            }
+        for(int nei : graph.get(node)){
+            if(!visited[nei]) dfs(visited,graph,nei);
         }
     }
 }
