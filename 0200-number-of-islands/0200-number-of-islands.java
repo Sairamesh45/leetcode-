13986c96ce24519e2aa715a08874dce3f655c8a5
Sai ramesh <sairameshdevaguptapu@gmail.com>
2026-09-09 22:33:52 +0530

    Time: 8 ms (10.48%), Space: 51.3 MB (92.16%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,5 +1,9 @@
 class Solution {
     public int numIslands(char[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+
+
         int dirs[][] = {
             {-1, 0},
             {1, 0},
@@ -7,8 +11,6 @@ class Solution {
             {0, 1}
         };
 
-        int n = grid.length;
-        int m = grid[0].length;
         int count = 0;
 
         for(int i = 0; i < n; i++)
@@ -17,24 +19,26 @@ class Solution {
             {
                 if(grid[i][j] == '1')
                 {
+                    grid[i][j] = '0';
                     count++;
                     Queue<int[]> q = new LinkedList<>();
+
                     q.offer(new int[]{i, j});
-                    grid[i][j] = '0';
 
                     while(!q.isEmpty())
                     {
                         int curr[] = q.poll();
+
                         int r = curr[0];
                         int c = curr[1];
 
-                        for(int dir[] : dirs)
+                        for(int dir[]: dirs)
                         {
                             int nr = r + dir[0];
                             int nc = c + dir[1];
 
-                            if(nr >= 0 && nc >= 0 && nr < n && nc < m && grid[nr][nc] == '1')
-                            {   
+                            if(nr >= 0 && nr < n && nc >= 0 && nc < m && grid[nr][nc] == '1')
+                            {
                                 grid[nr][nc] = '0';
                                 q.offer(new int[]{nr, nc});
                             }
@@ -43,6 +47,7 @@ class Solution {
                 }
             }
         }
+
         return count;
     }
 }
