23a902516bec12faa100230f7fc614a0e36696fd
Sai ramesh <sairameshdevaguptapu@gmail.com>
2026-09-09 20:57:23 +0530

    Time: 7 ms (14.47%), Space: 51.2 MB (92.16%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        int dirs[][] = {
+            {-1, 0},
+            {1, 0},
+            {0, -1},
+            {0, 1}
+        };
+
+        int n = grid.length;
+        int m = grid[0].length;
+        int count = 0;
+
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < m; j++)
+            {
+                if(grid[i][j] == '1')
+                {
+                    count++;
+                    Queue<int[]> q = new LinkedList<>();
+                    q.offer(new int[]{i, j});
+                    grid[i][j] = '0';
+
+                    while(!q.isEmpty())
+                    {
+                        int curr[] = q.poll();
+                        int r = curr[0];
+                        int c = curr[1];
+
+                        for(int dir[] : dirs)
+                        {
+                            int nr = r + dir[0];
+                            int nc = c + dir[1];
+
+                            if(nr >= 0 && nc >= 0 && nr < n && nc < m && grid[nr][nc] == '1')
+                            {   
+                                grid[nr][nc] = '0';
+                                q.offer(new int[]{nr, nc});
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return count;
+    }
+}
