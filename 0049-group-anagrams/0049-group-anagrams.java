55075614d1413a344015c1519dbdb82279112595
Sai ramesh <sairameshdevaguptapu@gmail.com>
2026-09-20 22:05:19 +0530

    Time: 19 ms (16.25%), Space: 51.1 MB (6.77%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,28 +1,31 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        HashMap<String,List<String>> map = new HashMap<>();
-        
+        HashMap<String, List<String>> map = new HashMap<>();
+
         for(int i = 0; i < strs.length; i++)
         {
             int freq[] = new int[26];
             for(int j = 0; j < strs[i].length(); j++)
             {
-                freq[strs[i].charAt(j) - 'a']++;
+                char ch = strs[i].charAt(j);
+                freq[ch - 'a']++;
             }
-
+            
             String str = Arrays.toString(freq);
 
-            if(map.containsKey(str))
-            {
-                map.get(str).add(strs[i]);
-            }
-            else{
-                List<String> arr = new ArrayList<>();
-                map.put(str,arr);
-                map.get(str).add(strs[i]);
-            }
+            if(!map.containsKey(str))
+                map.put(str, new ArrayList<>());
+            
+            map.get(str).add(strs[i]);
         }
 
-        return new ArrayList<>(map.values());
+        List<List<String>> arr = new ArrayList<>();
+
+        for(String i : map.keySet())
+        {
+            arr.add(map.get(i));
+        }
+
+        return arr;
     }
 }
